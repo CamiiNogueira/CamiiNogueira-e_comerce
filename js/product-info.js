59e48a7f6e84a443d48811e9fb39f98d0cc0eb34
JuliGaar <julietafuentesquintana@gmail.com>
2024-09-30 15:19:31 -0300

    Primer fetch comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -55,11 +55,41 @@ function showData(product) {
     }); 
 }
 
+function showComments(comments){
+    const comentariosDiv = document.getElementById('calificaciones-container');
+    comentariosDiv.innerHTML = '';
+    comments.forEach(comment => {
+        comentariosDiv.innerHTML += `
+        <div class="comentario">
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#007bff"></rect>
+        <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
+        </svg>
+            <div class="usuario">${comment.user}</div>
+            <div class="fecha">${comment.dateTime}</div>
+            <div class="comentario">${comment.description}</div>
+            <div class="calificacion-estrellas">
+                    ${Array.from({ length: 5 }, (_, i) => 
+                        `<span class="star ${i < comment.score ? 'filled' : ''}">★</span>`
+                    ).join('')}
+            </div>
+        </div>
+        <hr>
+        `;
+    }); 
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
     const productId = localStorage.getItem('prodID');
-    getJSONData(`${PRODUCT_INFO_URL}/${productId}.json`).then(function(resultObj){
+    getJSONData(`${PRODUCT_INFO_URL}${productId}.json`).then(function(resultObj){
         if (resultObj.status === "ok"){
             showData(resultObj.data)
+        }
+    });
+    getJSONData(`${PRODUCT_INFO_COMMENTS_URL}${productId}.json`).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            showComments(resultObj.data)
         }
     });
 });
