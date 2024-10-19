4dc46168022d7adb3a87a6afd25c8f67aea9bd69
JuliGaar <julietafuentesquintana@gmail.com>
2024-10-19 15:59:23 -0300

    Arreglitos validación
    Eliminación de los estilos :invalid para evitar que se vean a penas entrar a la página (antes de enviar el formulario), modificación del botón cancelar, re-ordenamiento de inputs, modificación para que solo se guarden los datos si son válidos

--- js/my-profile.js
+++ js/my-profile.js
@@ -4,20 +4,23 @@
     var forms = document.getElementsByClassName('needs-validation');
     Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
-        // Almacenar datos en localStorage
-        localStorage.setItem('nombre', document.getElementById('nombre').value);
-        localStorage.setItem('snombre', document.getElementById('snombre').value);
-        localStorage.setItem('apellido', document.getElementById('apellido').value);
-        localStorage.setItem('sapellido', document.getElementById('sapellido').value);
-        localStorage.setItem('telefono', document.getElementById('telefono').value);
-        localStorage.setItem('email', document.getElementById('email').value);
-
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
+        } else {
+          //Si el formulario es válido entonces almacenar datos en localStorage
+          localStorage.setItem('nombre', document.getElementById('nombre').value);
+          localStorage.setItem('snombre', document.getElementById('snombre').value);
+          localStorage.setItem('apellido', document.getElementById('apellido').value);
+          localStorage.setItem('sapellido', document.getElementById('sapellido').value);
+          localStorage.setItem('telefono', document.getElementById('telefono').value);
+          localStorage.setItem('email', document.getElementById('email').value);
         }
         form.classList.add('was-validated');
       }, false);
+      document.getElementById('cancelar').addEventListener('click', function(event){
+          location.reload();
+      });
     });
   }, false);
 })();
