ec9364818aa3222d98d088b5831e5e43a7e74244
JuliGaar <julietafuentesquintana@gmail.com>
2024-11-02 09:28:42 -0300

    Calcular-total

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,8 @@
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
 // Función para cargar los productos desde localStorage y mostrarlos en la tabla
 function cargarCarrito() {
     console.log("Ejecutando cargarCarrito...");
-    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
     let tabla = document.getElementById('cart-items');
 
     // Limpiar la tabla antes de agregar los productos
@@ -20,7 +21,7 @@ function cargarCarrito() {
                         <button class="quantity-btn">+</button>
                     </div>
                 </td>
-                <td>${producto.moneda} ${producto.precio * producto.cantidad}</td>
+                <td class="subtotal">${producto.moneda} ${producto.precio * producto.cantidad}</td>
                 <td><button class="delete-btn"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                     </svg></button></td>
@@ -28,9 +29,44 @@ function cargarCarrito() {
         `;
         tabla.innerHTML += fila;
     });
+    calcularTotal();
 }
 
 // Cargar los productos al cargar la página
 window.onload = function() {
     cargarCarrito();
+    document.querySelectorAll('input[name="opcion"]').forEach((input) => {
+        input.addEventListener('change', calcularTotal);
+    });
+};
+
+
+function calcularTotal(){
+    if (document.getElementById('pesos').checked) {
+        let total = 0;
+        carrito.forEach(producto => {
+            let precio = producto.precio;
+            if (producto.moneda != 'UYU'){
+                precio *= 41;
+            }
+            let subtotal = precio * producto.cantidad;
+            total += subtotal;
+        });
+        document.getElementById('total').value = 'UYU ' + total;
+        document.getElementById('subtotal').value = 'UYU ' + total;
+    }
+    else{
+        document.getElementById('dolares').checked = true;
+        let total = 0;
+        carrito.forEach(producto => {
+            let precio = producto.precio;
+            if (producto.moneda != 'USD'){
+                precio *= 0.02;
+            }
+            let subtotal = precio * producto.cantidad;
+            total += subtotal;
+        });
+        document.getElementById('total').value = 'USD ' + total;
+        document.getElementById('subtotal').value = 'USD ' + total;
+    }
 };
